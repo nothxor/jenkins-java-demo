b37e97d806f6444e7dc481ac2c81a440a537b823
nothxor <68539772+nothxor@users.noreply.github.com>
2025-08-15 16:21:50 +0200

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins Pipeline!");
+        System.out.println("Java application deployed to k3s cluster successfully!");
+    }
+}
